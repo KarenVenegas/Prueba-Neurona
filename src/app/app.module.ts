0f0dbfec81f7e59537171e742fa5c5f63c1e43bc
KarenVenegas <karencitavc@gmail.com>
2023-03-09 09:19:52 -0500

    Creación de componentes, módulos y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { EmpleadoComponent } from './components/empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    ListEmpleadosComponent,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule,
